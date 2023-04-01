c6f59e4a532a473879c09d26a7e3fb247c5c1267
SkidHub-SkeletonHDSky <70666835+SkidHub-SkeletonHDSky@users.noreply.github.com>
2023-04-02 00:47:21 +0200

    Update SkidhubLUA.lua

--- SkidhubLUA.lua
+++ SkidhubLUA.lua
@@ -1,3 +1,7 @@
-if PlaceId == 12802235086 then
+local gameid = game.PlaceId
+
+if(gameid == 12802235086) then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/SkidHub-SkeletonHDSky/STAFFSkidHub/main/InfiniteRarities'))()
+else
+  print("Game not founded")
 end
