1501ad43a66f6ad0f26150aba97248056c60c8fe
SkidHub-SkeletonHDSky <70666835+SkidHub-SkeletonHDSky@users.noreply.github.com>
2023-04-05 19:22:44 +0200

    Update SkidhubLUA.lua

--- SkidhubLUA.lua
+++ SkidhubLUA.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 12802235086 then
         loadstring(game:HttpGet('https://raw.githubusercontent.com/SkidHub-SkeletonHDSky/STAFFSkidHub/main/InfiniteRarities'))()
 elseif game.PlaceId == 12398038681 then
         loadstring(game:HttpGet('https://raw.githubusercontent.com/SkidHub-SkeletonHDSky/STAFFSkidHub/main/GartenofBanban'))()
+elseif game.PlaceId == 10968929125 then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/SkidHub-SkeletonHDSky/STAFFSkidHub/main/RarityRumble'))()
 end
