80fe10c6f358bf3ff88c56aa9705db5db6c23e03
SkidHub-SkeletonHDSky <70666835+SkidHub-SkeletonHDSky@users.noreply.github.com>
2023-04-06 16:27:58 +0200

    Update SkidhubLUA.lua

--- SkidhubLUA.lua
+++ SkidhubLUA.lua
@@ -1,7 +1,12 @@
-if game.PlaceId == 12802235086 then
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/SkidHub-SkeletonHDSky/STAFFSkidHub/main/InfiniteRarities'))()
-elseif game.PlaceId == 12398038681 then
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/SkidHub-SkeletonHDSky/STAFFSkidHub/main/GartenofBanban'))()
-elseif game.PlaceId == 10968929125 then
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/SkidHub-SkeletonHDSky/STAFFSkidHub/main/RarityRumble'))()
-end
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.2.5) ~  Much Love, Ferib 
+
+]]--
+
+if (game.PlaceId==(12802235754 -(181 + 487))) then loadstring(game:HttpGet("https://raw.githubusercontent.com/SkidHub-SkeletonHDSky/STAFFSkidHub/main/InfiniteRarities"))();elseif (game.PlaceId==12398038681) then loadstring(game:HttpGet("https://raw.githubusercontent.com/SkidHub-SkeletonHDSky/STAFFSkidHub/main/GartenofBanban"))();elseif (game.PlaceId==(11814501801 -845572676)) then loadstring(game:HttpGet("https://raw.githubusercontent.com/SkidHub-SkeletonHDSky/STAFFSkidHub/main/RarityRumble"))();end
